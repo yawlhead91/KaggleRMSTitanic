a383b9f7c8886e26bd11857f6a70247dc4422660
yawlhead91 <alexncork91@gmail.com>
2017-10-24 15:24:38 +0100

    impute adata

--- main.py
+++ main.py
@@ -8,17 +8,26 @@ Created on Tue Oct 24 14:27:39 2017
 
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import Imputer
 
 
 def main():
+    imputer = Imputer(missing_values = 'NaN', strategy='mean', axis=0)
+    
     # Read dataframes
-    traind = pd.read_csv("./train.csv")
-    testd = pd.read_csv("./test.csv")
+    df = pd.read_csv("./train.csv")
+    tdf = pd.read_csv("./test.csv")
     
     # Remove fetures that do not contribute 
     # to the prediction of the final class.
-    traind.drop(["Name", "Cabin", "Ticket"], axis=1)
-    testd.drop(["Name", "Cabin", "Ticket"], axis=1)
+    df = df.drop(["Name", "Cabin", "Ticket"],1)
+    tdf = tdf.drop(["Name", "Cabin", "Ticket"], 1)
+    
+    imputer.fit( df[['Age', 'Fare']] )
+    df['Age'] = imputer.transform( df[['Age', 'Fare']] )
+    df.dropna(axis=0,subset=['Embarked'])
+
+    print(df)
     
     
     return
