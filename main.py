6531ba2982d3362b10bd81ad232ccee4b35718d1
yawlhead91 <alexncork91@gmail.com>
2017-11-07 14:40:10 +0000

    Submission

--- main.py
+++ main.py
@@ -9,31 +9,35 @@ Created on Tue Oct 24 14:27:39 2017
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import Imputer
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import MinMaxScaler
+from sklearn import metrics
 
-def preProccess(df, tdf):
+def preProccess(df):
     # Imputation transformer for completing missing values.
     imputer = Imputer(missing_values = 'NaN', strategy='mean', axis=0)
+    scaler = MinMaxScaler()
     
     # Remove fetures that do not contribute 
     # to the prediction of the final class.
     df = df.drop(["Name", "Cabin", "Ticket"],1)
-    tdf = tdf.drop(["Name", "Cabin", "Ticket"], 1)
     
     imputer.fit( df[['Age', 'Fare']] )
-    df['Age'] = imputer.transform( df[['Age', 'Fare']] )
-
-    df.dropna(axis=0,subset=['Embarked'])
+    tf = imputer.transform( df[['Age', 'Fare']] )
+    df['Age'] = tf[:, 0]
+    df['Fare']= tf[:, 1]
+    
+    df = df.dropna(axis=0, subset=['Embarked'], how='all')
     
     # Encoding Categorical Variable
     sex_mapping = {'male':0, 'female':1}
     df['Sex'] = df['Sex'].map(sex_mapping)
-    tdf['Sex'] = df['Sex'].map(sex_mapping)
     
     emb_mapping = {'S': 0, 'C': 1, 'Q': 2} 
     df['Embarked'] = df['Embarked'].map(emb_mapping)
-    tdf['Embarked'] = df['Embarked'].map(emb_mapping)
     
-    return df, tdf
+    df[["Age", "SibSp", "Parch", "Fare", "Pclass"]] = scaler.fit_transform(df[["Age", "SibSp", "Parch", "Fare", "Pclass"]])
+    return df
 
 
 
@@ -42,9 +46,10 @@ def main():
     df = pd.read_csv("./train.csv")
     tdf = pd.read_csv("./test.csv")
     
-    df, tdf = preProccess(df, tdf)
-    
-    dft = df['Survived']
+    df = preProccess(df)
+    tdf = preProccess(tdf)
+    #print(df)
+    dfc = df['Survived']
     df = df.drop(['Survived'], 1)
     
     dfid = df['PassengerId']
@@ -53,9 +58,14 @@ def main():
     tdfid = tdf['PassengerId']
     tdf = tdf.drop(['PassengerId'], 1)
     
+    neigh = KNeighborsClassifier(n_neighbors=3)
+    neigh.fit(df, dfc) 
     
+    results = neigh.predict(tdf)
     
-    print(dft)
+    resultSeries = pd.Series(data = results, name = 'Survived', dtype='int64')
+    df = pd.DataFrame({"PassengerId":tdfid, "Survived":resultSeries})
+    df.to_csv("submission.csv", index=False, header=True)
     
     return
 
